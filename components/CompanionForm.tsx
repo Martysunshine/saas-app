453b854ec4e8bab91ed1e28b7f86a6338d4fe5b1
Martysunshine <smrdici.koala@gmail.com>
2025-07-09 22:50:32 +0200

    implement supabase along with clerk and create companion server action

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -17,7 +17,8 @@ import {
 import { Input } from "@/components/ui/input"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { subjects } from "@/constants"
-
+import { createCompanion } from "@/lib/actions/companion.actions"
+import { redirect } from "next/navigation"
 
 const formSchema = z.object({
   name: z.string().min(1, {message: 'Companion is required.'}),
@@ -43,9 +44,17 @@ duration: 15,
   })
 
   // 2. Define a submit handler.
-const onSubmit = (values: z.infer<typeof formSchema>) => {
-  console.log(values)
+const onSubmit = async (values: z.infer<typeof formSchema>) => {
+  const companion = await createCompanion(values);
+
+  if (companion) {
+    redirect(`/companions/${companion.id}`);
+  } else {
+    console.log('Failed to create companion');
+    redirect('/');
 }
+}
+
   return (
 <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
